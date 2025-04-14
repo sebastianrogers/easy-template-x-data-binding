5fec97de3350f0909ef6691cd010fdaa0f129c08
Paul Matthews <6736509+pmatthews05@users.noreply.github.com>
2025-04-14 13:44:21 +0100

    added jest-html-reporters

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,15 @@ module.exports = {
   moduleDirectories: [path.resolve("."), "node_modules"],
   testRegex: "/test/.*[.]tests[.]ts$",
   snapshotSerializers: ["<rootDir>/test/xmlNodeSnapshotSerializer.ts"],
-  reporters: ["default", ["jest-junit", { outputDirectory: "test-reports" }]],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "test-reports" }],
+    ["jest-html-reporters", {
+      publicPath: "./test-reports",
+      filename: "report.html",
+      urlForTestFiles: "https://github.com/sebastianrogers/easy-template-x-data-binding/blob/develop",
+      inlineSource: true,
+    }]
+  ],
   watchPathIgnorePatterns: ["integration"]
 };
