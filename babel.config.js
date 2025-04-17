192252fc6875f83cae7177097982b00869a9c1fa
Sebastian Rogers <sebastian.rogers@simpleinnovation.co.uk>
2025-04-17 16:37:24 +0000

    Converted to esm

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
 const isJest = process.env.NODE_ENV === "test";
 
-module.exports = {
-  assumptions: {
-    setSpreadProperties: true
-  },
-  presets: ["@babel/typescript"],
-  plugins: [
-    "babel-plugin-ts-nameof",
-    "@babel/plugin-transform-class-properties",
-    ["@babel/plugin-transform-object-rest-spread", { "useBuiltIns": true }],
-    "@babel/plugin-transform-optional-catch-binding",
-    isJest && "@babel/transform-modules-commonjs"
-  ].filter(Boolean)
+export default {
+    assumptions: {
+        setSpreadProperties: true
+    },
+    presets: ["@babel/typescript"],
+    plugins: [
+        "babel-plugin-ts-nameof",
+        "@babel/plugin-transform-class-properties",
+        ["@babel/plugin-transform-object-rest-spread", { "useBuiltIns": true }],
+        "@babel/plugin-transform-optional-catch-binding",
+        isJest && "@babel/transform-modules-commonjs"
+    ].filter(Boolean)
 };
--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,24 @@
-const path = require("path");
+import path from "node:path";
 
-module.exports = {
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "js", "json"],
-  moduleDirectories: [path.resolve("."), "node_modules"],
-  testRegex: "/test/.*[.]tests[.]ts$",
-  snapshotSerializers: ["<rootDir>/test/xmlNodeSnapshotSerializer.ts"],
-  reporters: [
-    "default",
-    ["jest-junit", { outputDirectory: "test-reports" }],
-    ["jest-html-reporters", {
-      publicPath: "./test-reports",
-      filename: "report.html",
-      urlForTestFiles: "https://github.com/sebastianrogers/easy-template-x-data-binding/blob/develop",
-      inlineSource: true,
-    }]
-  ],
-  watchPathIgnorePatterns: ["integration"]
+export default {
+    testEnvironment: "node",
+    moduleFileExtensions: ["ts", "js", "json"],
+    moduleDirectories: [path.resolve("."), "node_modules"],
+    testRegex: "/test/.*[.]tests[.]ts$",
+    snapshotSerializers: ["<rootDir>/test/xmlNodeSnapshotSerializer.ts"],
+    reporters: [
+        "default",
+        ["jest-junit", { outputDirectory: "test-reports" }],
+        [
+            "jest-html-reporters",
+            {
+                publicPath: "./test-reports",
+                filename: "report.html",
+                urlForTestFiles:
+                    "https://github.com/sebastianrogers/easy-template-x-data-binding/blob/develop",
+                inlineSource: true,
+            },
+        ],
+    ],
+    watchPathIgnorePatterns: ["integration"],
 };
